640b8388246b0a0f30d524c4eabb59090945dbad
MoisesJPG <mojupega1610@gmail.com>
2023-12-18 10:50:12 +0100

    a

--- app.js
+++ app.js
@@ -1,60 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Abrir o crear la base de datos
-    const request = indexedDB.open("database", 1);
+let db;
 
-    request.onerror = function(event) {
-        console.log("Error al abrir la base de datos:", event.target.errorCode);
+function openDatabase() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'database.db', true);
+    xhr.responseType = 'arraybuffer';
+
+    xhr.onload = function (e) {
+        const uInt8Array = new Uint8Array(this.response);
+        db = new SQL.Database(uInt8Array);
+        console.log('Base de datos abierta con éxito');
     };
 
-    request.onupgradeneeded = function(event) {
-        // Crear un almacén de objetos (tabla) llamado "miAlmacen"
-        const db = event.target.result;
-        const miAlmacen = db.createObjectStore("miAlmacen", { keyPath: "id", autoIncrement: true });
-
-        // Puedes agregar índices u otros ajustes aquí si es necesario
-    };
-
-    request.onsuccess = function(event) {
-        console.log("Base de datos abierta con éxito");
-        db = event.target.result;
-    };
-});
-
-function addData() {
-    const transaction = db.transaction(["miAlmacen"], "readwrite");
-    const miAlmacen = transaction.objectStore("miAlmacen");
-
-    // Agregar datos de ejemplo
-    const data = { mensaje: "¡Hola, mundo!" };
-    const request = miAlmacen.add(data);
-
-    request.onsuccess = function(event) {
-        console.log("Datos agregados con éxito");
-    };
-
-    request.onerror = function(event) {
-        console.log("Error al agregar datos:", event.target.errorCode);
-    };
+    xhr.send();
 }
 
-function readData() {
-    const transaction = db.transaction(["miAlmacen"], "readonly");
-    const miAlmacen = transaction.objectStore("miAlmacen");
+function queryDatabase() {
+    if (!db) {
+        console.error('La base de datos no está abierta. Asegúrate de abrir la base de datos primero.');
+        return;
+    }
 
-    // Leer todos los datos
-    const request = miAlmacen.openCursor();
+    // Ejemplo de consulta SQL
+    const query = 'SELECT * FROM tuTabla';
+    const result = db.exec(query);
 
-    request.onsuccess = function(event) {
-        const cursor = event.target.result;
-        if (cursor) {
-            console.log("ID:", cursor.key, "Mensaje:", cursor.value.mensaje);
-            cursor.continue();
-        } else {
-            console.log("Fin de los datos");
-        }
-    };
-
-    request.onerror = function(event) {
-        console.log("Error al leer datos:", event.target.errorCode);
-    };
+    console.log('Resultado de la consulta:', result);
 }
+
+document.addEventListener('DOMContentLoaded', openDatabase);
